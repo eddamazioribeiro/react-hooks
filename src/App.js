aa157546697d7f753cd62b6198f3aeb7456f0565
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-04 22:37:35 -0300

    Created simple method do increment / clear counter, setting the state.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component{
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  };
+
+  clearCount = () => {
+    this.setState({
+      count: 0
+    })  
+  };
+
   render () {
     return (
-      <h2>Counter App</h2>
+      <div>
+        <h2>Counter App</h2>
+        <button onClick={this.increment}>Clicked {this.state.count} times</button>
+        <button onClick={this.clearCount}>Clear counter</button>
+      </div>
     );
   };
 }
