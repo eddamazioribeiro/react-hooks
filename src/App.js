3dbf5a297c711e5050706b895eedafa65c50defa
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-05 21:40:44 -0300

    Using lifecycle methods componentDidMount() and componentDidUpdate() in the class component way.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import React, { Component, useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+// const App = () => {
+//   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  }
+//   const increment = () => {
+//     setCount(count + 1);
+//   }
 
-  const clearCount = () => {
-    setCount(0);
-  }
+//   const clearCount = () => {
+//     setCount(0);
+//   }
 
-  return (
-    <div>
-      <h2>Counter App</h2>
-      <button onClick={increment}>Clicked {count} times</button>
-      <button onClick={clearCount}>Clear counter</button>
-    </div>
-  );  
+//   return (
+//     <div>
+//       <h2>Counter App</h2>
+//       <button onClick={increment}>Clicked {count} times</button>
+//       <button onClick={clearCount}>Clear counter</button>
+//     </div>
+//   );  
+// }
+
+class App extends Component{
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  };
+
+  clearCount = () => {
+    this.setState({
+      count: 0
+    })  
+  };
+
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`;
+  }; 
+
+  componentDidUpdate() {
+    document.title = `Clicked ${this.state.count} times`;
+  }; 
+
+  render () {
+    return (
+      <div>
+        <h2>Counter App</h2>
+        <button onClick={this.increment}>Clicked {this.state.count} times</button>
+        <button onClick={this.clearCount}>Clear counter</button>
+      </div>
+    );
+  };
 }
 
 export default App;
