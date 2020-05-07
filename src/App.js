50afc2a22ce9cab57f8f5a1a35284efbe207f03d
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-06 21:51:25 -0300

    Controlling useEffect's behaviour on search, handling the submit event.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React, { Component, useState, useEffect } from 'react';
 const App = () => {
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=react');
+
   const fetchNews = () => {
-    fetch(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(err => console.error(err));
@@ -12,16 +14,21 @@ const App = () => {
 
   useEffect(() => {
     fetchNews();
-  }, [searchQuery]);
+  }, [url]);
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+
   return(
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           value={searchQuery}
